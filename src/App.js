b6bd8976201fbbef2b0b30737ee208909661f90f
quizitocristiano <quizitocristiano10@gmail.com>
2022-10-20 15:41:41 -0300

    move rotas para app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Home } from "./pages/home";
+import { Logo } from "./componente/logo";
+
+import "./styles/styles.css";
+import { Login } from "./pages/login";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Logo />
+
+      <div className="contariner">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
