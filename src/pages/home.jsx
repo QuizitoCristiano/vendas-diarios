85c84ca5b00995506f557c692d9d8345175e827c
quizitocristiano <quizitocristiano10@gmail.com>
2022-10-20 15:50:51 -0300

    simplifica a home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "../styles/styles.css";
 
-import { Logo } from "../componente/logo";
-import { Headecontete } from "../componente/headecontete";
-import {Logofinal} from "../componente/logofinal";
+import { HeadContent } from "../componente/headecontete";
+import { Logofinal } from "../componente/logofinal";
 export const Home = () => {
   return (
     <>
-      <Logo />
-      <div className="contariner">
-        <Headecontete />
-        <Logofinal/>
-      </div>
+      <HeadContent />
+      <Logofinal />
     </>
   );
 };
